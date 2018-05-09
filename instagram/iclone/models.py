4dcb661fa3e2faf30b0dda6755c507edc0270543
ppolle <peter.m.polle@gmail.com>
2018-05-09 14:24:45 +0300

    implemented all necessary image upload configurations

--- instagram/iclone/models.py
+++ instagram/iclone/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
-	profile_pic = models.ImageField(upload_to = 'profile', blank = True)
+	profile_pic = models.ImageField(upload_to = 'profile/', blank = True)
 	bio = models.TextField(blank = True)
 	user = models.ForeignKey(User, on_delete = models.CASCADE)
 
--- instagram/iclone/urls.py
+++ instagram/iclone/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import url
 from . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns=[
     url('^$',views.index,name = 'index'),
     url(r'^accounts/profile',views.profile,name = 'profile'),
     
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
