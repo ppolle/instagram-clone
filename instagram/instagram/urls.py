fcfc63088795715a30cc77a9f7ebc565074c8bb6
ppolle <peter.m.polle@gmail.com>
2018-05-09 12:10:32 +0300

    implemented registration functionality

--- instagram/instagram/urls.py
+++ instagram/instagram/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-     url(r'^',include('iclone.urls')),
+    url(r'^',include('iclone.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
 ]
